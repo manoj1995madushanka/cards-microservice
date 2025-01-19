0ff8b2f535f6fb66db8d7c727d42fc00aa348054
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-19 22:43:43 +0530

    implement cards microservice from template

---
+++ src/main/java/com/example/cards/dto/ResponseDto.java
@@ -0,0 +1,39 @@
+package com.example.cards.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response information"
+)
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+    @Schema(
+            description = "Status code in the response"
+    )
+    private String statusCode;
+
+    @Schema(
+            description = "Status message in the response"
+    )
+    private String statusMsg;
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatusMsg() {
+        return statusMsg;
+    }
+
+    public void setStatusMsg(String statusMsg) {
+        this.statusMsg = statusMsg;
+    }
+}
